ff6457c2611a5f55ab19e7e9b06aa4114c313bab
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-02-07 11:08:13 +0100

    Remove mocks for browser-specific symbols in tests

--- test/setup-dom.ts
+++ test/setup-dom.ts
@@ -10,15 +10,3 @@ axe.configure({
     },
   ],
 });
-
-HTMLCanvasElement.prototype.getContext = (() => {}) as any;
-HTMLDialogElement.prototype.showModal = function() {
-  this.setAttribute('open', '');
-};
-HTMLDialogElement.prototype.close = function() {
-  this.removeAttribute('open');
-  this.dispatchEvent(new CloseEvent('close'));
-  this.dispatchEvent(new CloseEvent('cancel'));
-};
-
-window.matchMedia = () => ({ matches: false }) as any;
